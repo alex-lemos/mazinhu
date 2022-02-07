b94e41f5afc6f4417fc1724fb558715774b151a6
alex-lemos <75213249+alex-lemos@users.noreply.github.com>
2022-02-07 18:23:40 -0300

    mazinhuu

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let image = document.querySelector('img');
+let nome = document.querySelector('h1');
+let botao = document.querySelector('button')
+
+let mazinho = "MAZINHUUU"
 
 function happy(){
-    image.setAttribute("src", "./assets/giphy.gif")
+    image.setAttribute("src", "./assets/giphy.gif");
+    nome.innerHTML = "MAZINHUUUUUUU"
+    botao.style.display = "none"
 }
     
